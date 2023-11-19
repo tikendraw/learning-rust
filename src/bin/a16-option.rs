96e09c2c02ac716a7932521ae18e1d01f44536c9
tikendra <tikendraksahu1029@gmail.com>
2023-11-19 07:59:37 +0530

    Option exercise

---
+++ src/bin/a16-option.rs
@@ -0,0 +1,27 @@
+// Topic: Option
+//
+// Requirements:
+// * Print out the details of a student's locker assignment
+// * Lockers use numbers and are optional for students
+//
+// Notes:
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+
+struct Student{
+    name: String,
+    locker: Option<i32>
+}
+
+fn main() {
+    let students = vec![
+        Student{name: "aarambh".to_owned(), locker: Some(12)},
+        Student{name: "anant".to_owned(), locker: None}
+    ];
+    for student in students {
+        match student.locker{
+            Some(num) => println!("Name: {:?}, Locker: {:?}", student.name, num),
+            None => println!("Name: {:?}, Locker: Locker not assigned", student.name)
+        }
+    }
+}
